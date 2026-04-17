2d68fd1a023cd4df817629adfb11121d973c15bf
ayoub804 <mraihiayoub123@gmail.com>
2026-04-17 17:39:03 +0100

    fix: hardcode JWT expiresIn to 30d

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -2,7 +2,7 @@ const User = require('../models/User');
 const jwt = require('jsonwebtoken');
 
 const signToken = (id) =>
-    jwt.sign({ id }, process.env.JWT_SECRET, { expiresIn: process.env.JWT_EXPIRE });
+    jwt.sign({ id }, process.env.JWT_SECRET, { expiresIn: '30d' });
 
 // POST /api/auth/register
 exports.register = async (req, res) => {
